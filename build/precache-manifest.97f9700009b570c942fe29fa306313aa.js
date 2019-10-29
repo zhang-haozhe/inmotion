e197461935abea380a2f0594c5bc1180f46bc7e8
Jason Chen <cyhy462702086@gmail.com>
2019-10-29 19:27:09 -0400

    working boilerplate

---
+++ build/precache-manifest.97f9700009b570c942fe29fa306313aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8036c5a430895eaa436a7c53888cb73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4882ae4059cde5f0b481",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "bcc269d1a62ff5196ddf",
+    "url": "/static/css/main.9b7579e8.chunk.css"
+  },
+  {
+    "revision": "4882ae4059cde5f0b481",
+    "url": "/static/js/2.d25e5b7c.chunk.js"
+  },
+  {
+    "revision": "bcc269d1a62ff5196ddf",
+    "url": "/static/js/main.5e12af3a.chunk.js"
+  },
+  {
+    "revision": "0f22f7e697ec619ebcd1",
+    "url": "/static/js/runtime~main.a82acdc9.js"
+  }
+]);
