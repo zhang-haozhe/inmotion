fdd8018788ee6b017523a1431abb649048c6c186
1264126181 <1264126181@qq.com>
2019-11-13 14:08:47 -0500

    update

---
+++ a.js
@@ -0,0 +1,3 @@
+let m = require('./scriptHomepage')
+
+console.log(x)
---
+++ scriptHomepage.js
@@ -0,0 +1,45 @@
+
+
+Promise.all([
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+    faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
+]).then();
+
+
+
+
+const img = document.getElementById('imageUpload');
+
+
+if (img) {
+    img.addEventListener('change', async () => {
+
+        image = await faceapi.bufferToImage(img.files[0])
+        Descriptor = loadLabeledImages(image);
+        console.log(Descriptor)
+    })
+}
+
+
+
+
+async function loadLabeledImages(image) {
+
+    const labels = ['owner']
+    return Promise.all(
+        labels.map(async label => {
+            const descriptions = []
+            for (let i = 1; i <= 1; i++) {
+
+                const detections = await faceapi.detectSingleFace(image).withFaceLandmarks().withFaceDescriptor()
+
+                descriptions.push(detections.descriptor)
+            }
+            console.log(1)
+            return new faceapi.LabeledFaceDescriptors(label, descriptions)
+        })
+    )
+}
+
