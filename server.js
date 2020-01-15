a8bb0c799b16af972df640a06cdabbe19d323adf
Haozhe Zhang <zhz19980514@ufl.edu>
2020-01-15 14:59:38 -0500

    Back end files pasted in

--- server.js
+++ server.js
@@ -1,36 +1,41 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*"),
-        res.setHeader("Access-Control-Allow-Header",
-            "Origin,X-Requested-With,Content-Type,Accept"
-        )
-    res.setHeader("Access-Control-Allow-Method",
-        "GET,POST,DELETE"
-    )
-    next();
-})
-app.use(cors())
+  res.setHeader("Access-Control-Allow-Origin", "*"),
+    res.setHeader(
+      "Access-Control-Allow-Header",
+      "Origin,X-Requested-With,Content-Type,Accept"
+    );
+  res.setHeader("Access-Control-Allow-Method", "GET,POST,DELETE");
+  next();
+});
+app.use(cors());
 
-let value = null
-app.post('/descriptor', (req, res) => {
-    let descriptor = req.body;
-    console.log(descriptor)
-    value = descriptor;
-})
+let value = null;
+app.post("/descriptor", (req, res) => {
+  let descriptor = req.body;
+  console.log(descriptor);
+  value = descriptor;
+});
 
-app.get('/get', (req, res) => {
-    console.log(value)
-    res.send(value)
-})
+app.get("/get", (req, res) => {
+  console.log(value);
+  res.send(value);
+});
 
 app.listen(port, () => {
-    console.log("server start")
-})
+  console.log("server start");
+});
