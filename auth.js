3162e212e4ef8199157fdbd0fff0ad09e1231eff
LutongZhang <aa1264126181@gmail.com>
2020-01-16 17:18:26 -0500

    login form

--- auth.js
+++ auth.js
@@ -2,34 +2,8 @@ import axios from "axios";
 import setAuthToken from "../utils/setAuthToken";
 
 //Login User
-export const login = (email, password) => async dispatch => {
-  const config = {
-    headers: {
-      "Content-Type": "application/json"
-    }
-  };
+const login = () => {
+  let elements = documents.getElementById("myForm").elements;
 
-  const body = JSON.stringify({ email, password });
-
-  try {
-    const res = await axios.post("/api/auth", body, config);
-
-    dispatch({
-      //   type: LOGIN_SUCCESS,
-      payload: res.data
-    });
-
-    dispatch(loadUser());
-  } catch (err) {
-    const errors = err.response.data.errors;
-    console.log(errors);
-
-    // if (errors) {
-    //   errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
-    // }
-
-    dispatch({
-      //   type: LOGIN_FAIL
-    });
-  }
-};
+  axios.post("http://localhost:8080/user", { a: 1 })
+}
--- server.js
+++ server.js
@@ -1,30 +1,32 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+//const connectDB = require('./config/db')
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 // Connect Database
-connectDB();
+//connectDB();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"),
-    res.setHeader(
-      "Access-Control-Allow-Header",
-      "Origin,X-Requested-With,Content-Type,Accept"
-    );
-  res.setHeader("Access-Control-Allow-Method", "GET,POST,DELETE");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*"),
+//     res.setHeader(
+//       "Access-Control-Allow-Header",
+//       "Origin,X-Requested-With,Content-Type,Accept"
+//     );
+//   res.setHeader("Access-Control-Allow-Method", "GET,POST,DELETE");
+//   next();
+// });
 app.use(cors());
 
 let value = null;
+
 app.post("/descriptor", (req, res) => {
   let descriptor = req.body;
   console.log(descriptor);
@@ -35,6 +37,10 @@ app.get("/get", (req, res) => {
   console.log(value);
   res.send(value);
 });
+
+app.post("/user", (req, res) => {
+  console.log(req.body)
+})
 
 app.listen(port, () => {
   console.log("server start");
