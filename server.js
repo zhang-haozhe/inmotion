a2ce1bdcca20f6e7a769ce79ca7ec3ce8d630d63
Haozhe Zhang <zhz19980514@ufl.edu>
2020-01-31 22:32:31 -0500

    hou duan

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-//const connectDB = require('./config/db')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const connectDB = require("./config/db");
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 // Connect Database
-//connectDB();
+connectDB();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+
 // app.use((req, res, next) => {
 //   res.setHeader("Access-Control-Allow-Origin", "*"),
 //     res.setHeader(
@@ -27,17 +34,17 @@ app.use(cors());
 
 let value = null;
 
-app.post('/descriptor', (req, res) => {
-	let descriptor = req.body;
-	console.log(descriptor);
-	value = descriptor;
+app.post("/descriptor", (req, res) => {
+  let descriptor = req.body;
+  console.log(descriptor);
+  value = descriptor;
 });
 
-app.get('/get', (req, res) => {
-	console.log(value);
-	res.send(value);
+app.get("/get", (req, res) => {
+  console.log(value);
+  res.send(value);
 });
 
 app.listen(port, () => {
-	console.log('server start');
+  console.log("server starting at " + port);
 });
