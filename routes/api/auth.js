bf8401b17e8c69137cf25dc393004f8079c1fa52
Haozhe Zhang <zhz19980514@ufl.edu>
2020-02-01 22:28:11 -0500

    Revert "Merge branch 'UserAuth' into ZLTsingleDetect"
    This reverts commit eec92004008778a6759374d81b213d804a32bfd5.

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -18,13 +18,13 @@ router.post(
     check("password", "Password is required").exists()
   ],
   async (req, res) => {
+    console.log(1)
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
     const { email, password } = req.body;
-    console.log(req.body);
 
     try {
       let user = await User.findOne({ email });
