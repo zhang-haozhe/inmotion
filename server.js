fb2804687411dc5be6ba6d52fcf0369e871161db
Jason Chen <cyhy462702086@gmail.com>
2020-02-02 01:20:47 -0500

    fixed stacked pics and alert

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const connectDB = require("./config/db");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const connectDB = require('./config/db');
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -12,16 +12,16 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '10mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
 // Define Routes
 app.use(cors());
-app.use("/api/users", require("./routes/api/users"));
-app.use("/api/auth", require("./routes/api/auth"));
-app.use("/api/profile", require("./routes/api/profile"));
-app.use("/api/posts", require("./routes/api/posts"));
-app.use("/api/img", require("./routes/api/img"));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/img', require('./routes/api/img'));
 
 // app.use((req, res, next) => {
 //   res.setHeader("Access-Control-Allow-Origin", "*"),
@@ -35,17 +35,17 @@ app.use("/api/img", require("./routes/api/img"));
 
 let value = null;
 
-app.post("/descriptor", (req, res) => {
-  let descriptor = req.body;
-  console.log("desc", descriptor);
-  value = descriptor;
+app.post('/descriptor', (req, res) => {
+	let descriptor = req.body;
+	console.log('desc', descriptor);
+	value = descriptor;
 });
 
-app.get("/get", (req, res) => {
-  console.log("value", value);
-  res.send(value);
+app.get('/get', (req, res) => {
+	console.log('value', value);
+	res.send(value);
 });
 
 app.listen(port, () => {
-  console.log("server start");
+	console.log('server start');
 });
